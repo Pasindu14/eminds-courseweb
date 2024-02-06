275014deccdaf54db29fdfb1a7a9e8c179219c4a
Pasindu Dulanjaya <145634886+Pasindu14@users.noreply.github.com>
2024-02-06 09:58:14 +0530

    asdsd

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,25 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
 export default withAuth(
+
+    function middleware(req) {
+        console.log(req.nextauth);
+        if (
+            req.nextauth.token?.role != "ADMIN"
+        ) {
+            return new NextResponse("You are not authorized!");
+        }
+    },
     {
         callbacks: {
-            authorized: ({ token }) => {
-                if (token?.role == "ADMIN") {
-                    return true;
-                }
-                return false;
+            authorized: (params) => {
+                let { token } = params;
+                return !!token;
             },
         },
     }
-)
+);
+
 
 export const config = { matcher: ["/badges", "/batches", "/courses", "/events", "/exam-results", "/exams", "/expire-badges", "/final-exams-submissions-results", "/jobs", "/payment-report", "/payments", "/questions", "/sessions", "/student-mapping", "/students"] }
