0aa415024677e6a6103fb87951a89f231d5a6b25
Pasindu Dulanjaya <145634886+Pasindu14@users.noreply.github.com>
2024-05-06 12:50:01 +0530

    Update auth.action.ts

--- server/actions/auth.action.ts
+++ server/actions/auth.action.ts
@@ -206,7 +206,7 @@ export async function resetFingerprint(userId: string) {
 
         let { error } = await supabaseCacheFreeClient
             .from('fingerprint')
-            .update([{ user_id: userId, fingerprint_01: null, fingerprint_02: null, reset_count: resetCount + 1 }]).eq('user_id', userId);
+            .update([{ user_id: userId, fingerprint_01: null, fingerprint_02: null, fingerprint_03: null, reset_count: resetCount + 1 }]).eq('user_id', userId);
         if (error) {
             throw error;
         }
